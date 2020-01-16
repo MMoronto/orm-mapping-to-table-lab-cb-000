85b8f2a9b5036f3f6fadecd652ec13d834c9c4b6
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-01-16 02:32:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,9 +2,10 @@ class Student
 
   attr_accessor :name, :grade, :id 
   
-  def initialize(name, grade)
+  def initialize(name, grade, id=nil)
     @name = name
     @grade = grade
+    @id = id 
   end
   
   def id 
