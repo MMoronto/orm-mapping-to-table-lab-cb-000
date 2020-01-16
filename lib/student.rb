fa52820dfdbbd708b52ceafa229a45120df99e0f
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-01-16 02:52:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,7 @@
 class Student
 
-  attr_accessor :name, :grade, :id 
+  attr_accessor :name, :grade
+  attr_reader :id 
   
   def initialize(name, grade, id=nil)
     @name = name
@@ -21,14 +22,16 @@ class Student
   end 
   
   def self.drop_table
-    
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
   end 
   
   def save 
     sql = <<-SQL
       INSERT INTO students (name, grade)
       VALUES (?, ?)
-    SQL 
+    SQL
+    
     DB[:conn].execute(sql, self.name, self.grade)
     
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
